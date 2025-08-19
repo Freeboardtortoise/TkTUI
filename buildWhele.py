183b69f8b64404ac43f96739a5b7dbca9276579e
CodeSandbox <support@codesandbox.io>
2025-08-19 12:03:54 +0000

    Initial commit

--- buildWhele.py
+++ buildWhele.py
@@ -4,14 +4,14 @@ import subprocess
 setup_py = '''from setuptools import setup, find_packages
 
 setup(
-    name="cursesui",
+    name="tkurses",
     version="0.1.0",
     author="Freeboardtortoise",
     author_email="Freeboardtortoise@gmail.com",
     description="Tkinter-like themed UI framework for curses",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/cursesui",
+    url="https://not-aplicable.com",
     packages=find_packages(),
     python_requires='>=3.6',
 )
