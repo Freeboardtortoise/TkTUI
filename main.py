6de6efcf038e43e18029270a28363e951caccc41
CodeSandbox <support@codesandbox.io>
2025-08-19 18:11:05 +0000

    Initial commit

--- main.py
+++ main.py
@@ -3,11 +3,22 @@ import tkurses.core as core
 import tkurses.widgets as widgits
 from tkurses.themes import DarkTheme
 
+def on_buttonPress(app, buttonWhat):
+    app.add_widget(widgits.Label(f"button pressed {buttonWhat}",  10,5))
 def main(stdscr):
     screen = stdscr
     app = core.App(screen, DarkTheme())
     app.add_widget(widgits.Label("Hello world",  0,0))
-    app.add_widget(widgits.Button("press me", 1,0))
+    button1 = widgits.Button("press me", 0,1, on_press=lambda: on_buttonPress(app, "press me button"))
+    app.add_widget(button1)
+    button2 = widgits.Button("press me too", 0, 2, on_press=lambda: on_buttonPress(app, "press Me too button"))
+    app.add_widget(button2)
 
-    screen.getch()
+    while True:
+        key = screen.getch()
+
+
+        if button1.handle_input(key) == False:
+            button2.handle_input(key)
+
 curses.wrapper(main)
