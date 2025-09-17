2833a4d551a36c12a780d462f370724a7988d8a7
CodeSandbox <support@codesandbox.io>
2025-09-17 09:36:07 +0000

    Initial commit

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from tkurses.themes import DarkTheme
 
 def on_buttonPress(app, buttonWhat):
     app.add_widget(widgits.Label(f"button pressed {buttonWhat}",  10,5))
+
 def main(stdscr):
     screen = stdscr
     app = core.App(screen, DarkTheme())
@@ -13,12 +14,8 @@ def main(stdscr):
     app.add_widget(button1)
     button2 = widgits.Button("press me too", 0, 2, on_press=lambda: on_buttonPress(app, "press Me too button"))
     app.add_widget(button2)
-    input = widgits.InputBox(10,10,20,10)
-
-    while True:
-        key = screen.getch()
-
-        if button1.handle_input(key) == False:
-            button2.handle_input(key)
+    input = widgits.InputBox("testing",1,10,10,2)
+    app.add_widget(input)
+    app.mainloop()
 
 curses.wrapper(main)
--- tkurses/widgets/inputbox.py
+++ tkurses/widgets/inputbox.py
@@ -6,19 +6,20 @@ ENTER_KEYS = ['\n', '\r', 10, 13, curses.KEY_ENTER]
 BACKSPACE_KEYS = ['\b', '\x7f', 8, 127, curses.KEY_BACKSPACE]
 
 class InputBox(Widget):
-    def init(self, title,x,y,width,height):
+    def __init__(self, title,x,y,width,height):
         super().__init__(x,y)
         self.text=""
-        self.size = (width,height)
+        self.size = [width,height]
         self.title=title
         self.done = False
     def render(self):
         self.app.theme.get_color("default")
-        rectangle(self.app.stdscr,self.x,self.y,self.x+self.size[0],self.y+self.size[1])
-        self.app.stdscr.addstr(self.y,self.x,self.title)
+        rectangle(self.app.stdscr,self.y,self.x,self.y+self.size[1],self.x+self.size[0])
+        self.app.stdscr.addstr(self.y,self.x+2,self.title)
+        self.app.stdscr.addstr(self.y+1,self.x+1,self.text)
     def handle_input(self,key):
-        if not self.done:
-            if key in typeable_chars:
+        if self.done == False:
+            if key in typeable_chars or chr(key) in typeable_chars:
                 self.text = self.text + chr(key)
             if key in BACKSPACE_KEYS:
                 self.text = self.text[:-1]
